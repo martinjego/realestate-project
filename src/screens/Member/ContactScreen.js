15f04c458d85ace86c2e7aadfee02bab3f236248
Martin Jego Alberto <martsalberto@gmail.com>
2018-10-17 18:15:18 +0800

    contact page

--- src/screens/Member/ContactScreen.js
+++ src/screens/Member/ContactScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 import MemberHeader from '../../components/Member/Header';
-import styles from '../../styles/home';
+import styles from '../../styles/contact';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class ContactScreen extends Component {
@@ -10,14 +10,41 @@ export default class ContactScreen extends Component {
       <Image source={require('../../img/mail-icon.png')} style={{tintColor}} />
     )
   }
+  constructor(){
+    super();
+
+    this.state = {
+      password: '',
+      new_password: ''
+    }
+  }
+
   render() {
-    const { navigate } = this.props.navigation
-    return (
+    const { navigate, goBack } = this.props.navigation
+    return(
       <View style={styles.container}>
-        <MemberHeader title="Contact Us"/>
-        <ScrollView>
-          <Text>Contact</Text>
-        </ScrollView>
+        <MemberHeader title="Contact Us" close={goBack}/>
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={this.state.password}
+              onChangeText={password => this.setState({ password })}
+              style={{ flex: 4, textAlign: 'center' }}
+              placeholder="Call 0998 966 02155"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={this.state.new_password}
+              onChangeText={password => this.setState({ new_password })}
+              style={{ flex: 4, textAlign: 'center' }}
+              placeholder="Email spinvest@gmail.com"
+            />
+          </View>
+        </View>
+        <TouchableOpacity onPress={() => navigate('Profile')} style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>OK</Text>
+        </TouchableOpacity>
       </View>
     )
   }
